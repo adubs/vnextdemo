e2312b2a4c362dc313862752fbba2cbf30ca1dfa
andy.white <andy.white@JEUKFPHLP1677.just-eat.local>
2014-10-28 18:11:52 +0000

    firstcommit

---
+++ src/Unit/HomeControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using vnextdemo.Controllers;
+using Xunit;
+
+namespace Unit
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void ControllerTest()
+        {
+            var controller = new HomeController();
+        }
+
+    }
+}
